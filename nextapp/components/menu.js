c1f0149136c81d88f8af421b5b925c3743b6fbf6
elrizal <erin@lrizal.com>
2019-08-29 23:57:12 -0400

    material ui

---
+++ nextapp/components/menu.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import IconButton from '@material-ui/core/IconButton';
+
+export default function MenuBtn() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  function handleClick(event) {
+    setAnchorEl(event.currentTarget);
+  }
+
+  function handleClose() {
+    setAnchorEl(null);
+  }
+
+  return (
+    <div>
+   
+      <IconButton
+      aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+          >
+          </IconButton>
+    
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+    </div>
+  );
+}
--- nextapp/pages/index.js
+++ nextapp/pages/index.js
@@ -1,9 +1,16 @@
 import Header from '../components/header'
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import NavBar from '../components/navbar';
 
 const Home = () => (
   <>
+  <NavBar/>
+  <Container maxWidth="sm">
+  <CssBaseline />
     <Header />
     <h1>Hello World!</h1>
+    </Container>
   </>
 )
 
